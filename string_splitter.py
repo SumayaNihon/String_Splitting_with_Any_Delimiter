5bb6faa7c895eea2e33941495f029ca8e85aa9ff
SumayaNihon <141727250+SumayaNihon@users.noreply.github.com>
2023-08-11 10:33:47 +0000

    Refactoring The Code

--- string_splitter.py
+++ string_splitter.py
@@ -1,8 +1,8 @@
 import re
 
-def split_string_with_multiple_delimiters(delimiters, input_string):
+def split_string_with_any_delimiters(delimiters, input_string):
     fields = re.split('[' + re.escape(''.join(delimiters)) + '\\s]+', input_string)
     non_empty_fields = [field for field in fields if field]
     print(non_empty_fields)
 
-split_string_with_multiple_delimiters(['sdf', ' ', ';', ':'], 'sdfkdjsadfsd diweiw;1231:foo')
+split_string_with_any_delimiters(['sdf', ' ', ';', ':'], 'sdfkdjsadfsd diweiw;1231:foo')
